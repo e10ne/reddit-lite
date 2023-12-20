d1a396e84adc668ecc490baaaf9133d2a95bc78f
e10ne <etiennevanhalteren@hotmail.com>
2023-12-20 14:15:42 +0100

    Create page to create subreddit

--- resources/views/subreddit.blade.php
+++ resources/views/subreddit.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
-    <div class="p-4 text-lime-700">
-        <h2 class="text-5xl">Subreddit</h2>
-        @foreach ($subreddits as $item)
-            <p>{{ $item->title }}</p>
-        @endforeach
-    </div>
+    <h2 class="text-4xl text-lime-700">Subreddit</h2>
+    @foreach ($subreddits as $item)
+        <p>{{ $item->title }}</p>
+    @endforeach
 </x-app-layout>
